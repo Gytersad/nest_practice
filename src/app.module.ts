e64474b28f07eb65e12ae3def01c31ca3f0ad087
Yakov Yuzhin <gytersad07@gmail.com>
2022-02-01 20:08:28 +0300

    4. Подключение к БД

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [],
+    providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'root',
+          database: 'nest-practice',
+          entities: [],
+          synchronize: true,
+        }),
+        UsersModule,
+      ],
+    
 })
 export class AppModule {}
