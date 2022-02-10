db40f19cb7e85d666538933803132fa1f00e7e5d
Yakov Yuzhin <gytersad07@gmail.com>
2022-02-10 17:21:07 +0300

    11. Ручки ролей

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { Role } from './roles.model';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+
+    constructor(@InjectRepository(Role) private roleRepository: Repository<Role>,) {}
+
+    async createRole(dto: CreateRoleDto) {
+        const role = await this.roleRepository.save(dto);
+        return role;
+    }
+
+    async getRoleByValue (value: string) {
+        const role = await this.roleRepository.findOne({where:{value}})
+        return role;
+    }
+}
