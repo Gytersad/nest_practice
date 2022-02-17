695b08c969c867d702d2eea702a317cdbc166dd1
Yakov Yuzhin <gytersad07@gmail.com>
2022-02-17 15:27:36 +0300

    15. Role guard, role decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -20,7 +22,8 @@ export class UsersController {
     
     @ApiOperation({summary: 'Получене пользователей'})
     @ApiResponse({status: 200, type: [User]})
-    @UseGuards(JwtAuthGuard)
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard, JwtAuthGuard)
     @Get()
     async getall (): Promise<User[]> {
         return this.usersService.getAllUsers();
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -18,7 +18,7 @@ export class UsersService {
 
         const newUser = await this.userRepository.save(userDto);
 
-        const role = await this.roleService.getRoleByValue('USER')
+        const role = await this.roleService.getRoleByValue('ADMIN')
 
         const newUserWithRole = await this.userRepository.findOne(newUser.id, { relations: ["roles"] });
         newUserWithRole.roles.push(role)
