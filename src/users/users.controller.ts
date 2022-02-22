0e041d4a19aa8aabc0738388833b941e24f1ed5a
Yakov Yuzhin <gytersad07@gmail.com>
2022-02-22 10:42:28 +0300

    15. Выдача ролей и бан

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,8 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { Roles } from 'src/auth/roles.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -16,7 +18,7 @@ export class UsersController {
     @ApiOperation({summary: 'Создаине пользователя'})
     @ApiResponse({status: 200, type: User})
     @Post()
-    async create (@Body() userDto: CreateUserDto) {
+    async create(@Body() userDto: CreateUserDto) {
         return this.usersService.createUser(userDto);
     }
     
@@ -25,7 +27,25 @@ export class UsersController {
     @Roles('ADMIN')
     @UseGuards(RolesGuard, JwtAuthGuard)
     @Get()
-    async getall (): Promise<User[]> {
+    async getall(): Promise<User[]> {
         return this.usersService.getAllUsers();
+    }
+
+    @ApiOperation({summary: 'Выдать роль'})
+    @ApiResponse({status: 200, type: [User]})
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard, JwtAuthGuard)
+    @Post('/role')
+    async addRole(@Body() dto: AddRoleDto) {
+        return this.usersService.addRole(dto);
+    }
+
+    @ApiOperation({summary: 'Забанить пользователя'})
+    @ApiResponse({status: 200, type: [User]})
+    @Roles('ADMIN')
+    @UseGuards(RolesGuard, JwtAuthGuard)
+    @Post('/ban')
+    async ban(@Body() dto: BanUserDto) {
+        return this.usersService.ban(dto);
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { RolesService } from 'src/roles/roles.service';
 import { Repository } from 'typeorm';
+import { AddRoleDto } from './dto/add-role.dto';
+import { BanUserDto } from './dto/ban-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
@@ -27,14 +29,38 @@ export class UsersService {
         return newUserWithRole;
     }
 
-   async getAllUsers() : Promise<User[]> {
-       const users = await this.userRepository.find({relations: ["roles"]})
-       return users;
-   }
+    async getAllUsers() : Promise<User[]> {
+        const users = await this.userRepository.find({relations: ["roles"]})
+        return users;
+    }
 
-   async getUserByEmail(email:string){
-       const user = await this.userRepository.findOne({where: {email}, relations: ["roles"]})
-       return user
-   }
+    async getUserByEmail(email:string){
+        const user = await this.userRepository.findOne({where: {email}, relations: ["roles"]})
+        return user
+    }
+
+    async addRole (addRoleDto: AddRoleDto): Promise<AddRoleDto> {
+        const user = await this.userRepository.findOne(addRoleDto.userId, { relations: ["roles"] })
+        const role = await this.roleService.getRoleByValue(addRoleDto.value)
+        if ( role && user) {
+            user.roles.push(role)
+            await this.userRepository.save(user);
+            return addRoleDto
+        }
+        throw new HttpException('Пользователь или роль не найдены', HttpStatus.NOT_FOUND)
+    }
+
+    async ban (banUserDto: BanUserDto): Promise<BanUserDto> {
+        const user = await this.userRepository.findOne(banUserDto.userId, { relations: ["roles"] })
+        if (user) {
+            user.roles = []
+            user.banned = true
+            user.banReason = banUserDto.banReason
+            await this.userRepository.save(user);
+            return banUserDto
+        }
+        throw new HttpException('Пользователь не найден', HttpStatus.NOT_FOUND)
+
+    }
 
 }
